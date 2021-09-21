9f3b60e01bf679fd3c709ea4a67662950b2e9f34
Michel Smola <michel.smola@farbenmeer.de>
2021-09-21 13:45:00 +0200

    chore(linter): Set up eslint and prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,40 @@
-import Image, { ImageProps } from "next/image";
-import React from "react";
+import NextImage, { ImageProps } from 'next/image'
+import React from 'react'
 
 export type NextImageFromFileProps = Partial<ImageProps> &
-  Pick<ImageProps, "src">;
+  Pick<ImageProps, 'src'>
 
 export default function NextImageFromFile(props: NextImageFromFileProps) {
   const [image, setImage] = React.useState<null | {
-    width: number;
-    height: number;
-  }>(null);
-  const imageRef = React.useRef<null | HTMLImageElement>(null);
+    width: number
+    height: number
+  }>(null)
+  const imageRef = React.useRef<null | HTMLImageElement>(null)
 
   // In case the image was cached and won't trigger onLoad
   // read width and height from the ref
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   React.useEffect(() => {
     // image already there or not rendered yet?
-    if (image || !imageRef.current) return;
+    if (image || !imageRef.current) return
     // image has not been loaded yet?
-    if (imageRef.current.complete === false) return;
+    if (imageRef.current.complete === false) return
     // width and height are 0 so the image has not been painted yet?
-    if (imageRef.current.width === 0 || imageRef.current.height === 0) return;
+    if (imageRef.current.width === 0 || imageRef.current.height === 0) return
 
     setImage({
       width: imageRef.current.width,
       height: imageRef.current.height,
-    });
-  });
+    })
+  })
 
   // Fall back to regular next/image if all necessary props are given
   if (
-    typeof props.src !== "string" ||
+    typeof props.src !== 'string' ||
     (props.width && props.height) ||
-    props.layout === "fill"
+    props.layout === 'fill'
   ) {
-    return <Image {...props} unoptimized />;
+    return <NextImage {...props} alt={props.alt} unoptimized />
   }
 
   // No width and height available? Render raw img tag first to figure them out
@@ -41,22 +42,24 @@ export default function NextImageFromFile(props: NextImageFromFileProps) {
     return (
       <img
         src={props.src}
+        alt={props.alt}
         ref={imageRef}
         onLoad={(event) => {
-          const target = event.target as HTMLImageElement;
-          setImage({ width: target.width, height: target.height });
+          const target = event.target as HTMLImageElement
+          setImage({ width: target.width, height: target.height })
         }}
       />
-    );
+    )
 
   // Width and height have been figured out, render the next/image
   return (
-    <Image
+    <NextImage
       {...props}
+      alt={props.alt}
       src={props.src}
       width={image.width}
       height={image.height}
       unoptimized
     />
-  );
+  )
 }
